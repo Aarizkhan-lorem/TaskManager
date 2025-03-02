a4b4377419f6044f2d84c89b9298006de5327b34
Aarizkhan-lorem <aarizkhan.lorem@gmail.com>
2025-03-02 11:10:30 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="text-red-600 flex justify-center text-3xl">
+      <button className="">TASK MANAGER APP</button>
+    </div>
+  );
+}
+
+export default App
